a3a6cffc37b6611321df481cd79b87aafe4d5ab0
shubhamstr <shubhamsutar5799@gmail.com>
2022-01-20 18:54:34 +0530

    custom css cards

---
+++ html_css_cards/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section class="ptr-1">
+    <div class="container">
+        <div class="custom_card_clickable">
+            <div class="custom_card_clickable-box">
+                <div class="icon-wrapper">
+                    <img src="https://icon-library.com/images/archive-icon-png/archive-icon-png-8.jpg" alt="">
+                </div>
+                <div class="custom_card_clickable-text">
+                    <h3>Import stylesheet <br>
+                        and data file
+                    </h3>
+                </div>
+            </div>
+            <div class="custom_card_clickable-box">
+                <div class="icon-wrapper">
+                    <img src="https://www.pngrepo.com/png/4046/512/archive.png" alt="">
+                </div>
+                <div class="custom_card_clickable-text">
+                    <h3>Import <br>
+                        Background plate
+                    </h3>
+                </div>
+            </div>
+            <div class="custom_card_clickable-box">
+                <div class="icon-wrapper">
+                    <img src="http://cdn.onlinewebfonts.com/svg/img_390595.png" alt="">
+                </div>
+                <div class="custom_card_clickable-text">
+                    <h3>Import sprite<br>
+                    </h3>
+                </div>
+            </div>
+        
+        </div>
+    </div>
+</section>
+</body>
+</html>
